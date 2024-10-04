1702e95cc9f5fce53317f8094f3ef10367e4afc6
Lezt Waper <leztwaper@gmail.com>
2024-10-04 20:48:54 +0700

    Fix return void of persistence method into task

--- CyberManager.Application/Common/Interfaces/Persistences/IBillRepository.cs
+++ CyberManager.Application/Common/Interfaces/Persistences/IBillRepository.cs
@@ -4,12 +4,12 @@ namespace CyberManager.Application.Common.Interfaces.Persistences;
 
 public interface IBillRepository
 {
-    void Create(Bill bill);
+    Task Create(Bill bill);
     Task<IEnumerable<Bill>> Get(
         BillType? type = null,
         DateOnly? dateStart = null,
         DateOnly? dateEnd = null);
     Task<Bill> GetById(int id);
-    void Update(Bill bill);
-    void Delete(Bill bill);
+    Task Update(Bill bill);
+    Task Delete(Bill bill);
 }
--- CyberManager.Application/Common/Interfaces/Persistences/IComputerRepository.cs
+++ CyberManager.Application/Common/Interfaces/Persistences/IComputerRepository.cs
@@ -4,12 +4,12 @@ namespace CyberManager.Application.Common.Interfaces.Persistences;
 
 public interface IComputerRepository
 {
-    void Create(Computer computer);
+    Task Create(Computer computer);
     Task<IEnumerable<Computer>> Get(
         string? name = null,
         int? coustPerHour = null,
         bool? isActive = null);
     Task<Computer> GetById(int id);
-    void Update (Computer computer);
-    void Delete(int id);
+    Task Update (Computer computer);
+    Task Delete(int id);
 }
