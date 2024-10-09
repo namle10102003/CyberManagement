4e11c1cb3beb0c62102637fb9c2c16a4150d8a86
Lezt Waper <leztwaper@gmail.com>
2024-10-09 13:53:27 +0700

    Feat: Add Repositories into Dependency Injection

--- CyberManager.Infrastructure/DependencyInjection.cs
+++ CyberManager.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using CyberManager.Application.Common.Interfaces.Persistences;
 using CyberManager.Infrastructure.Persistance.DataAccess;
+using CyberManager.Infrastructure.Persistance.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CyberManager.Infrastructure;
@@ -8,7 +10,20 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastucture(this IServiceCollection services, string connectionString)
     {
         //Add infracstucture service
+        services.AddPersistance(connectionString);
+        return services;
+    }
+
+    private static IServiceCollection AddPersistance(this IServiceCollection services, string connectionString)
+    {
         services.AddSingleton<IDataAccess>(_ => new DataAccess(connectionString));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ISoftwareRepository, SoftwareRepository>();
+        services.AddScoped<IBillRepository, BillRepository>();
+        services.AddScoped<IComputerRepository, ComputerRepository>();
+        services.AddScoped<IComputerErrorRepository, ComputerErrorRepository>();
+
         return services;
     }
 }
